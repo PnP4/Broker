a01c59122b602719c79c37821d87a1144ef59c24
NickHub4017 <nrv.rox.u@gmail.com>
2016-08-03 15:44:16 +0530

    Change initDB.py to work in class

--- initDB.py
+++ initDB.py
@@ -1,54 +1,62 @@
 import sqlite3
 
-conn = sqlite3.connect('test.db')
+
+class Database:
+    def __init__(self):
+        self.conn = sqlite3.connect('test.db')
+        self.createTable()
+
+    def checkRegistration(self, Topic):
+        cur = self.con.cursor()
+        params = (Topic,)
+        cur.execute('Select * from Service where "Topic" = ?', params)
+        result = cur.fetchone()
+        print result
+        cur.close()
+        self.con.commit()
+        if (result != None):
+            return True
+        else:
+            return False
 
 
-def checkRegistration(con,Topic):
-    cur = con.cursor()
-    params=(Topic,)
-    cur.execute('Select * from Service where "Topic" = ?',params)
-    result=cur.fetchone()
-    print result
-    cur.close()
-    con.commit()
-    if(result!=None):
-        return True
-    else:
-        return False
-
-
-def updateRegistration(con,Server,Topic):
-    cur = con.cursor()
-    cur.execute("UPDATE Service SET Server=? WHERE Topic=?", (Server, Topic))
-    cur.close()
-    con.commit()
+    def updateRegistration(self,Server,Topic):
+        cur = self.con.cursor()
+        cur.execute("UPDATE Service SET Server=? WHERE Topic=?", (Server, Topic))
+        cur.close()
+        self.con.commit()
 
 
 
 
-def createTable(conn):
-    try:
-        conn.execute('''CREATE TABLE Service
-               (ID INTEGER PRIMARY KEY  AUTOINCREMENT   NOT NULL,
-               Server           TEXT    NOT NULL,
-               Topic            TEXT     NOT NULL);''');
-    except Exception, err:
-        print err;
-    conn.commit()
+    def createTable(self):
+        try:
+            self.conn.execute('''CREATE TABLE Service
+                   (ID INTEGER PRIMARY KEY  AUTOINCREMENT   NOT NULL,
+                   Server           TEXT    NOT NULL,
+                   Topic            TEXT     NOT NULL);''');
+        except Exception, err:
+            print err;
+            self.conn.commit()
 
 
-def insertRegistration(con,Server,Topic):
-    if(not checkRegistration(con,Topic)):
-        cur = con.cursor()
-        cur.execute("insert into Service (Server,Topic) values (?, ?)", (Server, Topic))
-        con.commit()
-    else:
-        updateRegistration(con,Server,Topic)
+    def insertRegistration(self,Server,Topic):
+        if(not self.checkRegistration(self.con,Topic)):
+            cur = self.con.cursor()
+            cur.execute("insert into Service (Server,Topic) values (?, ?)", (Server, Topic))
+            self.con.commit()
+        else:
+            self.updateRegistration(self.con,Server,Topic)
 
 
-createTable(conn)
-insertRegistration(conn,"Helloppp",'WTF')
+    def closeconnection(self):
+        self.conn.close()
 
 
-conn.close()
+
+
+#insertRegistration(conn,"Helloppp",'WTF')
+
+
+
 
